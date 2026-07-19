04ba88758780cd777bc6e0c91eb807cee2aa5c78
xing meng <2086288361@qq.com>
2026-07-19 14:43:58 +0800

    小程序地址

--- sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,6 +30,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
     @Autowired
     private JwtTokenUserInterceptor jwtTokenUserInterceptor;
+
+    /**
+     * 开启尾部斜杠匹配
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
 
     /**
      * 注册自定义拦截器
